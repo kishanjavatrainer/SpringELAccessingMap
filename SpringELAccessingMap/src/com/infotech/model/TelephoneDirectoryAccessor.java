b34f79488c2ffee96eef49d4db4e3319446ee8fe
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-07 20:28:35 +0530

    SpringELAccessingMap source code

---
+++ SpringELAccessingMap/src/com/infotech/model/TelephoneDirectoryAccessor.java
@@ -0,0 +1,14 @@
+package com.infotech.model;
+
+public class TelephoneDirectoryAccessor {
+
+	private Integer telephoneNumber;
+	
+	public void setTelephoneNumber(Integer telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+	
+	public Integer getTelephoneNumber() {
+		return telephoneNumber;
+	}
+}
